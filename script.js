599de2892025ca8ecf56b6263ef47438a62427a2
Saumya Gaur <127776441+SaumyaG1318@users.noreply.github.com>
2023-07-29 15:22:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,27 @@ const text = document.querySelector(".row-4 p");
 button.addEventListener("click", checkMail);
 
 function checkMail(ev) {
-  let toCheck = email.value;
-  if (!toCheck.includes("@")) {
-    text.textContent = "Valid email required";
-    text.style.color = "red";
-    email.setAttribute("class", "em error");
-  } else {
-    text.style.color = "black";
-    text.textContent = "Email address";
-    email.setAttribute("class", "em ");
-    window.open("./assets/Sucess.html", "_self");
-    //for second html page
-    let insert = document.querySelector(".insert");
-    insert.textContent = toCheck;
+  if (ev) {
+    let toCheck = email.value;
+    localStorage.setItem("toInsert", email.value);
+    if (!toCheck.includes("@")) {
+      text.textContent = "Valid email required";
+      text.style.color = "red";
+      email.setAttribute("class", "em error");
+    } else {
+      text.style.color = "black";
+      text.textContent = "Email address";
+      email.setAttribute("class", "em ");
+      window.open("./assets/Sucess.html", "_self");
+    }
   }
 }
 
 const button2 = document.querySelector(".button-2");
+const Insert = document.querySelector(".insert");
+let x = localStorage.getItem("toInsert");
+Insert.insertAdjacentText("afterend", x);
 
 button2.addEventListener("click", ret);
 function ret(ev) {
-  window.open("../index.html", "_self");
-}
+  if (ev) window.open("../index.html", "_self");
